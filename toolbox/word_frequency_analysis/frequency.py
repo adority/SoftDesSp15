9c1c83e7af3f4718ba7f39b1501cc03b65c123f6
Aubrey Dority <aubrey.dority@students.olin.edu>
2015-03-13 21:35:09 -0400

    word frequency analysis of '20,000 Leagues Under the Sea'

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -9,9 +9,28 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	f = open(file_name,'r')
+	lines = f.readlines()
+	curr_line = 0
+	while lines[curr_line].find('CHAPTER 1') == -1:
+		curr_line += 1
+	lines = lines[curr_line+1:]
+	f.close()
 
-def get_top_n_words(word_list, n):
+	words = []
+	for line in lines:
+		for word in line.split():
+			word = word.strip(string.punctuation)
+			word = word.lower()
+			if word != '':
+				words.append(word)
+
+	return words
+
+#get_word_list('20000leagues.txt')
+
+
+def get_top_n_words(word_list,n):
 	""" Takes a list of words as input and returns a list of the n most frequently
 		occurring words ordered from most to least frequently occurring.
 
@@ -21,4 +40,15 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	frequencies = {}
+	for word in word_list:
+		if word in frequencies:
+			frequencies[word] += 1
+		else:
+			frequencies[word] = 1
+	ordered_by_frequency = sorted(frequencies, key=frequencies.get, reverse=True)
+	return ordered_by_frequency[0:n]
+
+word_list = get_word_list('20000leagues.txt')
+print get_top_n_words(word_list,100)
+
